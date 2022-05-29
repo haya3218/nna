ed30c962de166e95a10a77ca9688934167a9095b
hayayayay <74699483+haya3218@users.noreply.github.com>
2022-05-29 14:30:11 +0800

    small stuff

---
+++ src/AboutDialog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace nna_net3.src
+{
+    public partial class AboutDialog : Form
+    {
+        public AboutDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
